b23e07dfc1fd6de166acdb2241685652bbce6c24
stashchenkoksu <43968220+stashchenkoksu@users.noreply.github.com>
2020-08-26 17:11:03 +0300

    Delete task_2.rb

--- task_2.rb
+++
@@ -1,8 +0,0 @@
-# triangle square
-
-puts 'Enter triangle base'
-base = STDIN.gets.chomp.to_i
-puts 'Enter triangle hight'
-t_hight = STDIN.gets.chomp.to_i
-square = 0.5*base*t_hight
-puts square.to_s
