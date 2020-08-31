0e9622d8108e98dd49f55e8b16d9f603a6384d03
stashchenkoksu <43968220+stashchenkoksu@users.noreply.github.com>
2020-08-31 14:05:47 +0300

    complete 5-th task

---
+++ lesson_5/lib/station.rb
@@ -0,0 +1,60 @@
+require_relative 'train'
+require_relative 'route'
+require_relative 'instance_counter'
+class Station
+  include InstanceCounter
+  attr_reader :station_name
+  attr_accessor :trains, :all_created_stations
+
+  @@all_created_stations = []
+
+  def initialize station_name
+    @station_name = station_name
+    @trains = []
+    @@all_created_stations << self
+    self.register_instance
+    puts "Build station #{station_name}"
+  end
+
+  def self.all
+    puts "------------------"
+    puts "   All stations"
+    puts "------------------"
+    all_created_stations.each_with_index do |station, index|
+      puts "#{index}) station.station_name"
+    end
+    puts "------------------"
+  end
+
+
+  def arrive_train train
+    puts "On station #{station_name} arrives train №#{train.number}"
+    trains << train
+  end
+
+  def trains_on_station
+    puts 'On station right now:'
+    trains.each do |train|
+      puts train.number
+    end
+  end
+
+  def leave_station(train_number)
+    puts "From #{station_name} station leave train №#{train_number}"
+    trains.delete_if { |train| train_number == train.number }
+  end
+
+  def show_train_type_of(type = nil)
+    if type
+      puts "On the station #{station_name} trains type are: #{type}: "
+      trains.each do |train|
+        puts train.number if train.type == type
+      end
+    else
+      puts "Trains on the station #{station_name}: "
+      trains.each do |train|
+        puts train.number
+      end
+       end
+  end
+end
---
+++ lesson_5/lib/train.rb
@@ -0,0 +1,129 @@
+require_relative 'route'
+require_relative 'station'
+require_relative 'wagon'
+require_relative 'creator'
+require_relative 'instance_counter'   
+
+class Train
+  
+  include Creator
+  include InstanceCounter
+  attr_reader :number, :type
+  attr_accessor :route, :current_station, :tracks, :wagon, :speed, :all_trains
+
+  @@all_trains = []
+  def initialize(number, type)
+    @number = number
+    @type = type
+    @tracks = []
+    @speed = 0
+    @route = nil
+    @current_station = nil
+    @@all_trains << self
+    register_instance
+    puts "Created train № #{number}. Type: #{type}. Tracks amount: #{tracks.size}."
+  end
+
+  def self.find train_number
+    @@all_trains.each do |train|
+     return train if train.number == train_number
+    end
+    return nil
+  end
+
+  def speed_up
+    speed += speed_const
+    puts 'train speed up'
+  end
+
+  def slow_down
+    puts 'Train slow down'
+    speed -= speed_const
+    speed = 0 if speed.negative?
+  end
+
+  
+  def attech_track track
+    if speed.zero?
+      tracks << track
+      puts "amount of tracks : #{tracks.size}"
+    else
+      puts "train is mooving! you can't attach tracks"
+    end
+  end
+
+  def unhook_track track 
+    if speed.zero?
+      if tracks.size.positive?
+        tracks.delete(track)
+        puts "amount of tracks : #{tracks.size}"
+      else
+        puts "You can't delete wagon,becouse train doesn't have any"
+      end
+    else
+      puts "train is mooving! you can't unhook tracks"
+    end
+  end
+
+  def add_route(new_route)
+    puts "previous route: #{route}"
+    puts "new route: #{new_route}"
+    if route.nil?
+      self.route = new_route
+      route.start_station.arrive_train(self)
+      self.current_station = new_route.start_station
+    else
+      current_station.leave_station(number)
+      self.route = new_route
+      current_station = route.start_station
+      current_station.arrive_train(self)
+    end
+    puts " the route #{route.start_station.station_name} - #{route.end_station.station_name} appoint to the train №#{number} "
+    puts "current_station : #{self.current_station.station_name}"
+  end
+
+  def moove_to_the_next_statioin
+    if route.nil?
+      puts "this train doesn't have a route yet"
+    elsif current_station != route.end_station
+      current_station.leave_station(number)
+      self.current_station = route.inner_stations[route.inner_stations.index(current_station) + 1]
+      p
+      current_station.arrive_train self
+      puts "next station #{current_station.station_name}"
+    else
+      puts "You can't go to the next station, this train is alredy on the last station"
+    end
+  end
+
+  def moove_to_the_previous_station
+    if route.nil?
+      puts "this train doesn't have a route yet"
+    elsif current_station != route.start_station
+      current_station&.leave_station(number)
+      self.current_station = route.inner_stations[(route.inner_stations.index(current_station) - 1)]
+      current_station.arrive_train self
+      puts "next station #{current_station.station_name}"
+
+    else
+      puts "You can't go to the previous station, this train is alredy on the first station"
+    end
+  end
+
+ def show_tracks
+      puts "All wagons of train №#{number}"
+      puts "----------"
+      tracks.each_with_index do |track, index|
+        puts "#{index}) Number: #{track.number} | type: #{type} | manufactor: #{track.creator_name}"
+      end
+      puts "----------"
+    end
+
+  protected
+    def speed_const
+      20
+    end
+
+   
+
+end
